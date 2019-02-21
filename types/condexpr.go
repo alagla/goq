79ae84b52f6ed2b4eb7a28e38484c06f1c25b47d
lunfardo314 <evaldas.alt@gmail.com>
2019-02-21 09:00:27 +0200

    analyze sceleton

--- types/condexpr.go
+++ types/condexpr.go
@@ -4,8 +4,23 @@ type QuplaCondExpr struct {
 	If   *QuplaExpressionWrapper `yaml:"if"`
 	Then *QuplaExpressionWrapper `yaml:"then"`
 	Else *QuplaExpressionWrapper `yaml:"else"`
+	//--
+	ifExpr   ExpressionInterface
+	thenExpr ExpressionInterface
+	elseExpr ExpressionInterface
 }
 
-func (conExpr *QuplaCondExpr) Analyze(module *QuplaModule) (ExpressionInterface, error) {
-	return nil, nil // TODO
+func (e *QuplaCondExpr) Analyze(module *QuplaModule) (ExpressionInterface, error) {
+	var err error
+
+	if e.ifExpr, err = e.If.Analyze(module); err != nil {
+		return nil, err
+	}
+	if e.thenExpr, err = e.Then.Analyze(module); err != nil {
+		return nil, err
+	}
+	if e.elseExpr, err = e.Else.Analyze(module); err != nil {
+		return nil, err
+	}
+	return e, nil
 }
