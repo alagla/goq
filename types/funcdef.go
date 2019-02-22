5184dae78fb682c977ae34a782c8f50edf1cb82b
lunfardo314 <evaldas.alt@gmail.com>
2019-02-22 21:00:20 +0200

    state vars

--- types/funcdef.go
+++ types/funcdef.go
@@ -5,35 +5,52 @@ import (
 )
 
 type QuplaEnvStmt struct {
-	Name string `yaml:"Name"`
+	Name string `yaml:"name"`
 	Join bool   `yaml:"join"`
+}
+
+type QuplaNameExpr struct {
+	Size int64                   `yaml:"size"`
+	Type *QuplaExpressionWrapper `yaml:"type"`
+}
+
+type QuplaStateVar struct {
+	Size int64  `yaml:"size"`
+	Type string `yaml:"type"`
 }
 
 type QuplaFuncDef struct {
 	ReturnType     *QuplaExpressionWrapper            `yaml:"returnType"` // only size is necessary
-	Params         map[string]*QuplaExpressionWrapper `yaml:"params"`
+	Params         map[string]*QuplaNameExpr          `yaml:"params"`
+	State          map[string]*QuplaStateVar          `yaml:"state"`
 	Env            []*QuplaEnvStmt                    `yaml:"env,omitempty"`
 	Assigns        map[string]*QuplaExpressionWrapper `yaml:"assigns,omitempty"`
 	ReturnExprWrap *QuplaExpressionWrapper            `yaml:"return"`
 	//-------
 	analyzed bool
-	Name     string
+	name     string
 	retSize  int64
 	retExpr  ExpressionInterface
 	varScope map[string]*LocalVariable
 }
 
+const (
+	VARTYPE_ARG   = 0
+	VARTYPE_STATE = 1
+	VARTYPE_LOCAL = 2
+)
+
 // represents local variable or parameter in func def
 type LocalVariable struct {
 	name     string
-	isArg    bool
+	vartype  int
 	size     int64
 	expr     ExpressionInterface
 	analyzed bool
 }
 
 func (def *QuplaFuncDef) SetName(name string) {
-	def.Name = name
+	def.name = name
 }
 
 func (def *QuplaFuncDef) Analyze(module *QuplaModule) (*QuplaFuncDef, error) {
@@ -46,7 +63,7 @@ func (def *QuplaFuncDef) Analyze(module *QuplaModule) (*QuplaFuncDef, error) {
 	//debugf("Analyzing func def '%v'", def.Name)
 	defer func(perr *error) {
 		if *perr != nil {
-			errorf("Error while analyzing func def '%v': %v", def.Name, *perr)
+			errorf("Error while analyzing func def '%v': %v", def.name, *perr)
 		}
 	}(&err)
 
@@ -62,14 +79,18 @@ func (def *QuplaFuncDef) Analyze(module *QuplaModule) (*QuplaFuncDef, error) {
 	def.retSize = sz
 
 	// build var scope
-	def.createVarScope()
-
+	if err = def.createVarScope(); err != nil {
+		return nil, err
+	}
+	if err = def.analyzeAssigns(module); err != nil {
+		return nil, err
+	}
 	// return expression
 	if def.retExpr, err = def.ReturnExprWrap.Analyze(module, def); err != nil {
 		return nil, err
 	}
 	if def.retExpr == nil {
-		return nil, fmt.Errorf("in funcdef '%v': return expression can't be nil", def.Name)
+		return nil, fmt.Errorf("in funcdef '%v': return expression can't be nil", def.name)
 	}
 	return def, nil
 }
@@ -80,7 +101,7 @@ func (def *QuplaFuncDef) Size() int64 {
 
 func (def *QuplaFuncDef) InScope(name string) bool {
 	if def.varScope == nil {
-		panic(fmt.Errorf("var scope not ready for %v", def.Name))
+		panic(fmt.Errorf("var scope not ready for %v", def.name))
 	}
 	_, ok := def.varScope[name]
 	return ok
@@ -94,50 +115,66 @@ func (def *QuplaFuncDef) FindVar(name string, module *QuplaModule) (*LocalVariab
 	if ret.analyzed {
 		return ret, nil
 	}
-	var err error
-	if ret.isArg {
-		// param
-		if ret.expr == nil {
-			panic("ret.expr == nil")
-		}
-		t, err := ret.expr.Analyze(module, nil)
-		if err != nil {
-			return nil, err
-		}
-		if ret.size, err = GetConstValue(t); err != nil {
-			return nil, err
-		}
-		ret.expr = nil
-		ret.analyzed = true
-		return ret, nil
-	}
-	// local var
 	if ret.expr == nil {
 		ret.analyzed = true
+		ret.size = 0
 		return ret, nil // ???
 	}
-	if ret.expr, err = ret.expr.Analyze(module, def); err != nil {
-		return nil, err
+	var err error
+
+	switch ret.vartype {
+	case VARTYPE_LOCAL:
+		ret.analyzed = true
+		if ret.expr, err = ret.expr.Analyze(module, def); err != nil {
+			return nil, err
+		}
+		ret.size = ret.expr.Size()
+	case VARTYPE_STATE:
+		ret.analyzed = true
+		if ret.expr, err = ret.expr.Analyze(module, def); err != nil {
+			return nil, err
+		}
+		if ret.size != ret.expr.Size() {
+			return nil, fmt.Errorf("expression and state variable has different sizes in the assign")
+		}
 	}
-	ret.size = ret.expr.Size()
-	ret.analyzed = true
-	return ret, nil
+	return ret, nil // ???
 }
 
-func (def *QuplaFuncDef) createVarScope() {
+func (def *QuplaFuncDef) createVarScope() error {
 	def.varScope = make(map[string]*LocalVariable)
 	for name, t := range def.Params {
 		def.varScope[name] = &LocalVariable{
-			name:  name,
-			isArg: true,
-			expr:  t,
+			name:     name,
+			vartype:  VARTYPE_ARG,
+			size:     t.Size,
+			analyzed: true,
 		}
 	}
 	for name, a := range def.Assigns {
 		def.varScope[name] = &LocalVariable{
-			name:  name,
-			isArg: false,
-			expr:  a,
+			name:    name,
+			vartype: VARTYPE_LOCAL,
+			expr:    a,
 		}
 	}
+	for name, s := range def.State {
+		v, ok := def.varScope[name]
+		if ok && v.vartype == VARTYPE_ARG {
+			return fmt.Errorf("function parameter can't be declared state variable: '%v' in '%v'", name, def.name)
+		}
+		v.vartype = VARTYPE_STATE
+		v.size = s.Size
+	}
+	return nil
+}
+
+func (def *QuplaFuncDef) analyzeAssigns(module *QuplaModule) error {
+	var err error
+	for name := range def.Assigns {
+		if _, err = def.FindVar(name, module); err != nil {
+			return err
+		}
+	}
+	return nil
 }
--- types/sliceexpr.go
+++ types/sliceexpr.go
@@ -3,59 +3,32 @@ package types
 import "fmt"
 
 type QuplaSliceExpr struct {
-	Name          string                  `yaml:"name"`
-	StartExprWrap *QuplaExpressionWrapper `yaml:"start,omitempty"`
-	EndExprWrap   *QuplaExpressionWrapper `yaml:"end,omitempty"`
+	Var           string                  `yaml:"var"`
+	Offset        int64                   `yaml:"offset"`
+	SliceSize     int64                   `yaml:"size"`
+	StartExprWrap *QuplaExpressionWrapper `yaml:"start,omitempty"` // not used
+	EndExprWrap   *QuplaExpressionWrapper `yaml:"end,omitempty"`   // not used
 	//----
-	localVar  *LocalVariable
-	startExpr ExpressionInterface
-	sizeExpr  ExpressionInterface
-	start     int64
-	end       int64
+	localVar *LocalVariable
 }
 
 func (e *QuplaSliceExpr) Analyze(module *QuplaModule, scope *QuplaFuncDef) (ExpressionInterface, error) {
 	var err error
 
-	if e.localVar, err = scope.FindVar(e.Name, module); err != nil {
+	if e.localVar, err = scope.FindVar(e.Var, module); err != nil {
 		return nil, err
 	}
-
 	if e.localVar == nil {
 		var sc string
 		if scope != nil {
-			sc = scope.Name
+			sc = scope.name
 		} else {
 			sc = "nil"
 		}
-		return nil, fmt.Errorf("can't find local variable '%v' in scope '%v'", e.Name, sc)
+		return nil, fmt.Errorf("can't find local variable '%v' in scope '%v'", e.Var, sc)
 	}
-	if e.StartExprWrap != nil {
-		if e.startExpr, err = e.StartExprWrap.Analyze(module, scope); err != nil {
-			return nil, err
-		}
-		if e.start, err = GetConstValue(e.startExpr); err != nil || e.start < 0 || e.start >= e.localVar.size {
-			return nil, fmt.Errorf("wrong start offset in SliceExpr")
-		}
-		e.end = e.start + 1
-	} else {
-		e.startExpr = nil
-		e.start = 0
-		e.end = e.localVar.size
-		return e, nil
-	}
-	if e.EndExprWrap != nil {
-		if e.sizeExpr, err = e.EndExprWrap.Analyze(module, scope); err != nil {
-			return nil, err
-		}
-		var s int64
-		if s, err = GetConstValue(e.sizeExpr); err != nil || s > e.localVar.size {
-			return nil, fmt.Errorf("wrong slice size in SliceExpr")
-		}
-		e.end = e.start + s
-	} else {
-		e.sizeExpr = nil
-		e.end = e.start + 1
+	if e.Offset+e.SliceSize > e.localVar.size {
+		return nil, fmt.Errorf("wrong offset/size for the slice of '%v'", e.Var)
 	}
 	return e, nil
 }
@@ -64,5 +37,5 @@ func (e *QuplaSliceExpr) Size() int64 {
 	if e == nil {
 		return 0
 	}
-	return e.end - e.start
+	return e.SliceSize
 }
